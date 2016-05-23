a3557600e613875f8ecd5cc517fcc94c4570f2a3
Hugo Nordell <hugo@flowfeed.co>
2016-05-23 14:37:56 -0700

    Charts prettify.

--- public/main.js
+++ public/main.js
@@ -8,50 +8,10 @@ var socket = io();
 var connected = false;
 
 // Bar Variables
-var ctx, startingData, chart, latestLabel;
-var accData = [];
-function initChart() {
-  ctx = document.getElementById("acc-chart").getContext("2d");
-  startingData = {
-    labels: [1,2,3,4,5,6,7],
-    datasets: [
-      {
-        backgroundColor: "rgba(220,220,220,0.2)",
-        borderColor: "rgba(220,220,220,1)",
-        pointBackgroundColor: "rgba(220,220,220,1)",
-        pointStrokeColor: "#fff",
-        label: "X-acceleration",
-        data: [1,1,1,1,1,1,1]
-      },
-      {
-        backgroundColor: "rgba(151,187,205,0.2)",
-        borderColor: "rgba(151,187,205,1)",
-        pointBackgroundColor: "rgba(151,187,205,1)",
-        pointStrokeColor: "#fff",
-        label: "Y-acceleration",
-        data: [1,1,1,1,1,1,1]
-      },
-      {
-        backgroundColor: "rgba(100,187,125,0.2)",
-        borderColor: "rgba(123,55,205,1)",
-        pointBackgroundColor: "rgba(123,55,205,1)",
-        pointStrokeColor: "#fff",
-        label: "Z-acceleration",
-        data: [1,1,1,1,1,1,1]
-      }
-    ]
-  };
-  latestLabel = startingData.labels[6];
-  chart = new Chart(ctx, {
-    type: 'line',
-    data: startingData,
-    options: {
-      animationSteps: 15
-    }
-  });
-  console.log(chart);
-}
-initChart();
+var ctx1, startingAccData, accChart, latestAccLabel;
+var ctx2, startingGyroData, gyroChart, latestGyroLabel;
+initAccChart();
+initGyroChart();
 
 // Send message and emit 'message'
 function sendMessage() {
@@ -111,16 +71,106 @@ socket.on('phone', function(data) {
   $('#g').text('Gamma: ' + Math.round(data.gyro.gamma * 100)/100);
   
   // Remove old data and add new data
-  chart.data.datasets[0].data.shift();
-  chart.data.datasets[1].data.shift();
-  chart.data.datasets[2].data.shift();
-  chart.data.datasets[0].data.push(data.accelerometer.x);
-  chart.data.datasets[1].data.push(data.accelerometer.y);
-  chart.data.datasets[2].data.push(data.accelerometer.z);
-  chart.update();
+  accChart.data.datasets[0].data.shift();
+  accChart.data.datasets[1].data.shift();
+  accChart.data.datasets[2].data.shift();
+  accChart.data.datasets[0].data.push(data.accelerometer.x);
+  accChart.data.datasets[1].data.push(data.accelerometer.y);
+  accChart.data.datasets[2].data.push(data.accelerometer.z);
+  accChart.update();
+  
+  gyroChart.data.datasets[0].data.shift();
+  gyroChart.data.datasets[1].data.shift();
+  gyroChart.data.datasets[2].data.shift();
+  gyroChart.data.datasets[0].data.push(data.gyro.alpha);
+  gyroChart.data.datasets[1].data.push(data.gyro.beta);
+  gyroChart.data.datasets[2].data.push(data.gyro.gamma);
+  gyroChart.update();
 });
 
 // Tell clients when someone disconnects
 socket.on('left', function(msg) {
   $messages.append('<li>' + msg.message + '</li>');
 });
+
+// Graph stuff
+function initAccChart() {
+  ctx1 = document.getElementById("acc-chart").getContext("2d");
+  startingAccData = {
+    labels: [1,2,3,4,5,6,7],
+    datasets: [
+      {
+        backgroundColor: "rgba(234,108,108,0.2)",
+        borderColor: "rgba(234,108,108,1)",
+        pointBackgroundColor: "rgba(234,108,108,1)",
+        pointStrokeColor: "#fff",
+        label: "X-acceleration",
+        data: [1,1,1,1,1,1,1]
+      },
+      {
+        backgroundColor: "rgba(68,183,132,0.2)",
+        borderColor: "rgba(68,183,132,1)",
+        pointBackgroundColor: "rgba(68,183,132,1)",
+        pointStrokeColor: "#fff",
+        label: "Y-acceleration",
+        data: [1,1,1,1,1,1,1]
+      },
+      {
+        backgroundColor: "rgba(107,152,207,0.2)",
+        borderColor: "rgba(107,152,207,1)",
+        pointBackgroundColor: "rgba(107,152,207,1)",
+        pointStrokeColor: "#fff",
+        label: "Z-acceleration",
+        data: [1,1,1,1,1,1,1]
+      }
+    ]
+  };
+  latestAccLabel = startingAccData.labels[6];
+  accChart = new Chart(ctx1, {
+    type: 'line',
+    data: startingAccData,
+    options: {
+      animationSteps: 15
+    }
+  });
+}
+function initGyroChart() {
+  ctx2 = document.getElementById("gyro-chart").getContext("2d");
+  startingGyroData = {
+    labels: [1,2,3,4,5,6,7],
+    datasets: [
+      {
+        backgroundColor: "rgba(234,108,108,0.2)",
+        borderColor: "rgba(234,108,108,1)",
+        pointBackgroundColor: "rgba(234,108,108,1)",
+        pointStrokeColor: "#fff",
+        label: "Alpha",
+        data: [1,1,1,1,1,1,1]
+      },
+      {
+        backgroundColor: "rgba(68,183,132,0.2)",
+        borderColor: "rgba(68,183,132,1)",
+        pointBackgroundColor: "rgba(68,183,132,1)",
+        pointStrokeColor: "#fff",
+        label: "Beta",
+        data: [1,1,1,1,1,1,1]
+      },
+      {
+        backgroundColor: "rgba(107,152,207,0.2)",
+        borderColor: "rgba(107,152,207,1)",
+        pointBackgroundColor: "rgba(107,152,207,1)",
+        pointStrokeColor: "#fff",
+        label: "Gamma",
+        data: [1,1,1,1,1,1,1]
+      }
+    ]
+  };
+  latestGyroLabel = startingGyroData.labels[6];
+  gyroChart = new Chart(ctx2, {
+    type: 'line',
+    data: startingGyroData,
+    options: {
+      animationSteps: 15
+    }
+  });
+}
