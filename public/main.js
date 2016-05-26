2504edaa46fec83a639fc57713f0d7e04658a324
Hugo Nordell <hugo@flowfeed.co>
2016-05-26 09:40:49 -0700

    Android stuff.

--- public/main.js
+++ public/main.js
@@ -38,7 +38,9 @@ if (window.DeviceMotionEvent) {
       };
       socket.emit('phone', d);
     } else {
-      $('#phone-text').text('Your phone does not seem to send any actual sensor data. If you have an Android that might be the issue. Please try again with an iPhone.');
+      $('#phone-text').val('Your phone does not seem to send any actual sensor data. If you have an Android that might be the issue. Please try again with an iPhone.');
+      $('#phone-text-h2').val('Device not supported');
+      alert('Your device does not send sensor data. Please try with an iPhone or similar.');
     }
   }, false);
 }
